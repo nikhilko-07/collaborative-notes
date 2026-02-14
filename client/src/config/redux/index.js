4821e5e8168fc87843a773f4add4928501783e05
Nikhil Kohli <nikhilkohli2407@gmail.com>
2026-02-14 11:07:48 +0530

    Update index.js

--- client/src/config/redux/index.js
+++ client/src/config/redux/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const BASE_URL = "https://collaborative-notes-cx4x.vercel.app";
+export const BASE_URL = "https://collaborative-notes-cx4x.vercel.app/api";
 
 const clientServer = axios.create({
     baseURL: BASE_URL,
